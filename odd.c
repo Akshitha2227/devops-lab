763d41d40f9fefbb62a4a738c454fe481e137ea8
Akshitha2227 <245123751003@mvsrec.edu.in>
2026-03-09 16:02:40 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num=23
+
+    if(num % 2 == 0)
+        printf("%d is even.\n", num);
+    else
+        printf("%d is odd.\n", num);
+    
+    return 0;
+}
